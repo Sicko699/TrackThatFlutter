400b818f31aaf8fe66bb217ff4adb93953583c5f
Sicko699 <francesco.mattone.03@gmail.com>
2025-07-23 08:15:39 +0200

    ffff

--- lib/repositories/impl/authRepo_impl.dart
+++ lib/repositories/impl/authRepo_impl.dart
@@ -29,7 +29,8 @@ class AuthRepositoryImpl implements AuthRepository<UserModel> {
   @override
   Future<UserModel> register(String name, String email, String password) async {
     final data = await authService.register(name, email, password);
-
+    print("Risposta ricevuta dalla register: $data");
+    
     if (data == null || data['id'] == null || data['email'] == null || data['nome'] == null) {
       throw Exception("Registration failed or incomplete data");
     }
